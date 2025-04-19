7186eae9725a41a8650fa6af909048076b0456c7
kofteee <kerem.tufan@sabanciuniv.edu>
2025-04-19 23:11:35 +0300

    Delete lib directory

--- lib/product.dart
+++
@@ -1,9 +0,0 @@
-class Product{
-  String productName;
-  double percent;
-  double price;
-  String imageURL;
-  String property;
-  String saleURL;
-  Product({required this.productName, required this.percent,required this.price,required this.imageURL, required this.property, required this.saleURL});
-}
